c2e57f07880caa4b6b8ef2953bd39da2b0d488a3
Tung Pham <joseph.pham280996@gmail.com>
2020-12-11 19:52:06 -0500

    Fix Auth actions to send request to user.login

--- massenergize_carbon_calculator_app/src/actions/auth/index.js
+++ massenergize_carbon_calculator_app/src/actions/auth/index.js
@@ -1,44 +1,55 @@
 import _ from 'lodash'
-import * as types from '../types'
 import api from '../../api/massEnergize'
-// getUser action to get the user
-// TODO: May remove this action and just use userCreation action
-export const fetchUser = async user => {
-  // Attach email to request and send off to backend to get user info
-  const response = await api.get('/cc/info/user', {
+import generateFormData from '../../utils/generateFormData'
+
+export const fetchCCUser = async user => {
+  const idToken = await user.getIdToken(true)
+  const formData = generateFormData({ data: { idToken } })
+  const userProfileResponse = await api.post(`/v3/auth.login`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+
+  const CCUserProfile = await api.get('/v3/cc/info/user', {
     params: {
       email: user.email,
     },
   })
-  return _.get(response, 'data.data.userInfo')
-}
-// Handle Sign out action
-export const signOut = () => ({
-  type: types.SIGN_OUT,
-})
 
-// createUser action, send POST request to backend to save user registration info to database
+  return {
+    ..._.get(CCUserProfile, 'data.data'),
+    ..._.get(userProfileResponse, 'data.data'),
+  }
+}
+
+export const signOut = async () => {
+  const formData = generateFormData({})
+  await api.post(`/v3/auth.logout`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+}
+
 export const createUser = async (formValues, email, selected) => {
-  /* params :
-    first_name,
-    last_name,
-    email,
-    locality,
-    groups,
-    minimum_age,
-    accepts_terms_and_conditions */
   const params = {
     ...formValues,
     email,
     eventName: selected.name,
   }
-  // Get CSRF token
   const csrfResponse = await api.get(`/auth/csrf`)
   const { csrfToken } = csrfResponse.data.data
-  // Attach CSRF token to request's header and all information
-  const response = await api.post(`/cc/users`, {
-    ...params,
-    headers: { 'X-CSRFToken': csrfToken },
-  })
+  const response = await api.post(
+    `/cc/users`,
+    { ...params },
+    {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrfToken,
+      },
+    },
+  )
   return response.data.data
 }

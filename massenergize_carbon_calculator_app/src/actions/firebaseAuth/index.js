c472239584a4081305232db91e2fd61a7ec84e64
Tung Pham <joseph.pham280996@gmail.com>
2020-12-13 01:15:28 -0500

    Refactor Firebase actions

---
+++ massenergize_carbon_calculator_app/src/actions/firebaseAuth/index.js
@@ -0,0 +1,66 @@
+import firebase, {
+  googleProvider,
+  facebookProvider,
+} from '../../config/firebaseConfig'
+
+export const sendEmailVerification = ({ callback, onError }) => {
+  try {
+    firebase.auth().currentUser.sendEmailVerification()
+  } catch (err) {
+    onError(err)
+  } finally {
+    callback(false)
+  }
+}
+
+export const signInWithGoogle = ({ onSignInSuccess, onError }) => {
+  // Authentication reset upon closing tab/window
+  firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(() => {
+      firebase
+        .auth()
+        .signInWithPopup(googleProvider)
+        .then(async googleAuth => {
+          await onSignInSuccess(googleAuth)
+        })
+        .catch(err => {
+          onError(err)
+        })
+    })
+}
+// Sign In with Facebook function
+export const signInWithFacebook = ({ onSignInSuccess, onError }) => {
+  // Authentication reset upon closing tab/window
+  firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(() => {
+      firebase
+        .auth()
+        .signInWithPopup(facebookProvider)
+        .then(async facebookAuth => {
+          await onSignInSuccess(facebookAuth)
+        })
+        .catch(err => {
+          onError(err)
+        })
+    })
+}
+
+// Sign in with email and password function
+export const normalSignIn = (
+  { email, password },
+  { onSignInSuccess, onError },
+) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(async res => {
+      await onSignInSuccess(res)
+    })
+    .catch(err => {
+      onError(err)
+    })
+}
